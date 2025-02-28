46e615f28d2ac2867b9c385ec96173093e465d9d
loic <loic.pages01@etu.umontpellier.fr>
2025-02-28 14:22:18 +0100

    Nouvelle organisation des fichiers. Un dossier par trait d'histoire de vie, dans lequel on retrouve : - le fichier R qui permet de plot les modèles choisis - un dossier contenant : les plots des modèles + le PDF du fichier R précédent correspondant aux plots du dossier

---
+++ IPM/Predict.R
@@ -0,0 +1,91 @@
+####### Creation of a fake data set with responses predicted by the models ######
+
+#### Initialization
+library(spaMM)
+library(tidyverse)
+library(splines)
+
+setwd("/media/loic/Commun/0Travail/Stage 2025 ISEM/Models/IPM")
+
+centauree_data <- read.csv("donnesIPM_short.csv")
+centauree_data_complet <- read.csv("donnesIPM.csv")
+
+
+#Supprimer plantes dont l'age est inconnu
+centauree_data <- centauree_data[!is.na(centauree_data$age0), ]
+centauree_data$age1 <- ifelse(centauree_data$Stage1=="V",centauree_data$age0+1,NA)
+
+#Forcer l'age maximal à 8
+length(centauree_data$age0[centauree_data$age0 >= 8])
+centauree_data$age0[centauree_data$age0 > 8] <- 8
+
+spaMM.options(separation_max=70)
+
+## Creation of fake data
+AgeMax <- 8
+annees <- 1995:2022
+populations <- c("Po","Au","Pe","E1","E2","Cr")
+taille_range <- seq(0.5, 25, by = 0.5) 
+age_range <- 1:AgeMax
+
+fake_data <- expand.grid(
+  year = annees,
+  Pop = populations,
+  Size0Mars = NA,
+  age0 = age_range)
+
+## Add predicted responses
+
+# Fecondity
+cptldata <- centauree_data[!is.na(centauree_data$Cptl0),]
+cptldata <- cptldata[!cptldata$Flowering0==0,]
+
+Cptlglm1 <- fitme(Cptl0 ~ 1 + Size0Mars, 
+                  data=cptldata)
+Cptlpredict1 <- predict(Cptlglm1, newdata = fake_data)[,1]
+
+# Survival Probability
+survdata <- centauree_data[centauree_data$Flowering0!=1,]
+
+Survglm1 <- fitme(SurvieMars ~ 1+ bs(Size0Mars,df=5,degree=3) + bs(age0,degree=3,knots = 6.5)+ (Size0Mars|year) + (age0|Pop) ,
+                  family=binomial, 
+                  data=survdata,
+                  method="PQL/L")
+Survpredict1 <- predict(Survglm1, newdata = fake_data)[,1]
+
+# Growth
+growthdata <- centauree_data[!is.na(centauree_data$Size1Mars), ]
+growthdata <- growthdata[growthdata$Size1Mars != 0, ]
+
+Growthglm1 <- fitme(log(Size1Mars) ~ 1 + poly(log(Size0Mars),4) + poly(age0,3) + (log(Size0Mars)|year) + (log(Size0Mars)|Pop),
+                    data=growthdata)
+
+Growthpredict1 <- predict(Growthglm1, newdata = fake_data)[,1]
+
+# Flowering Probability
+Flowglm1 <- fitme(Flowering0 ~  1 + poly(Size0Mars,3) + poly(age0,2) + (age0|Pop),
+                  family=binomial,
+                  data=centauree_data, method="PQL/L")
+Flowpredict1 <- predict(Flowglm1, newdata = fake_data)[,1]
+
+
+# Add to fake_data
+fake_data <- fake_data %>% mutate(CapituleNbr = Cptlpredict1,
+                                  SurvivalProba = Survpredict1,
+                                  Size1 = exp(Growthpredict1),
+                                  FloweringProba = Flowpredict1)
+
+save(fake_data, file = "Prediction")
+
+save(Survglm1,Cptlglm1,Growthglm1,Flowglm1, file="Models")
+
+obs_beta=as.numeric(Flowglm1$fixef[1])
+se_obs_beta=as.numeric(sqrt(diag(vcov(Flowglm1)))[1])
+
+save(obs_beta, file = "obs_beta")
+save(se_obs_beta, file = "se_obs_beta")
+
+
+
+
+
